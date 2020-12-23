4c673936a7e884534157fe239fc45d3a29fcdb62
yujialves <yujialves@i.softbank.jp>
2020-12-23 13:34:56 +0900

    セレクタの更新

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 const repoTabSelector =
-  "#js-pjax-container > div.mt-4.position-sticky.top-0.d-none.d-md-block.bg-white.width-full.border-bottom.color-border-secondary > div > div > div.flex-shrink-0.col-12.col-md-9.mb-4.mb-md-0 > div > nav > a.UnderlineNav-item.selected";
+  "#js-pjax-container > div.mt-4.position-sticky.top-0.d-none.d-md-block.bg-white.width-full.border-bottom.color-border-secondary > div > div > div.flex-shrink-0.col-12.col-md-9.mb-4.mb-md-0 > div > nav > a:nth-child(2)";
 const repoHeaderButtonSelector =
   "body > div.position-relative.js-header-wrapper > header > div.Header-item.position-relative.mr-0.d-none.d-md-flex > details > details-menu > a:nth-child(6)";
 
